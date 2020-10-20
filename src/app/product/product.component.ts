9ebb588e988232f493c7ab17fad3741e5de323d5
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-20 19:58:15 +0100

    Add custom validation for the calendar and style it appropriately

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,13 @@
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+function minDateValidation(date: Date): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+  const forbidden = new Date(control.value) < date;
+  return forbidden ? {minDateValidation: {value: control.value}}
+  : null;
+  };
+ }
 
 @Component({
   selector: 'in-product',
@@ -7,7 +15,37 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./product.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class ProductComponent implements OnInit {
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      basic: fb.group({
+        name: ['', Validators.required],
+        description: ['', Validators.required],
+        active: false,
+        features: fb.array([
+          fb.control('')
+        ])
+      }),
+      expiration: fb.group({
+        expirationDate: [null, Validators.compose([Validators.required, minDateValidation(new Date())])]
+      })
+    });
+  }
+
+  get basicFeatures(): FormArray{
+    return this.productForm.get('basic.features') as FormArray;
+  }
+
+  // tslint:disable-next-line: typedef
+  get expirationError(){
+    if (this.productForm.get('expiration.expirationDate').hasError('required')){
+      return 'This Field Is Required!';
+    }
+    if (this.productForm.get('expiration.expirationDate').hasError('minDateValidation')){
+      return ' Expiration should be after today\'s date';
+    }
+  }
   deviceType ='tablet';
   deviceTypes =[{
     name: 'Tablet',
@@ -34,38 +72,21 @@ export class ProductComponent implements OnInit {
     icon: 'bicycle'
   }
 ];
+  productForm: FormGroup;
+  @Input() product;
+// tslint:disable-next-line: typedef
 selectDevice(device){
   this.deviceType = device.icon;
 }
-  productForm: FormGroup;
-  @Input() product;
-  constructor(private fb: FormBuilder) {
-    this.productForm = this.fb.group({
-      basic: fb.group({
-        name: ['', Validators.required],
-        description: ['', Validators.required],
-        active: false,
-        features: fb.array([
-          fb.control('')
-        ])
-      }),
-      expiration: fb.group({
-        expirationDate: null
-      })
-    });
-  }
 
   ngOnInit(): void {
   }
 
   // tslint:disable-next-line: typedef
   handleClose(){}
-
-  get basicFeatures(): FormArray{
-    return this.productForm.get('basic.features') as FormArray;
-  }
   // tslint:disable-next-line: typedef
   addFeature(){
     this.basicFeatures.push(this.fb.control(''))
   }
+
 }
